fcee52a2fdd718a0a3c862df0f54c62ba53bf7f1
Shakhov Pavel <pshakhov@fil-it.ru>
2023-10-26 19:57:27 +0300

    Initial authorization added

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  public email: string = '';
+  public password: string = '';
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login(): void {
+    this.authService.login(this.email, this.password).subscribe(() => {
+      /* TODO: change role assignment */
+      this.authService.userInfo
+        ? (this.authService.Role = this.authService.userInfo?.roles.map((item) => item.name))
+        : null;
+      alert(`current role is ${this.authService.role}`);
+    });
   }
 
-    protected readonly scroll = scroll;
+  protected readonly scroll = scroll;
 }
--- src/utils/types/types.ts
+++ src/utils/types/types.ts
@@ -3,3 +3,20 @@ export interface IMeetup {
   name: string;
   description: string;
 }
+
+export interface IUser {
+  email: string;
+  id: number;
+  roles: {
+    id: number;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+    userRole: {
+      id: number;
+      name: string;
+      createdAt: string;
+      updatedAt: string;
+    };
+  }[];
+}
