5ba81d6f3af75e8eb7b9e258e197b7fd337b29f0
Shakhov Pavel <pshakhov@fil-it.ru>
2023-10-30 16:16:21 +0300

    Guards added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { MeetupComponent } from './components/meetup/meetup.component';
 import { AboutComponent } from './components/about/about.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'meetups', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
-  { path: 'meetups', component: MeetupComponent },
+  { path: 'meetups', component: MeetupComponent, canActivate: [AuthGuard] },
   { path: 'about', component: AboutComponent },
 ];
 
--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -9,7 +10,10 @@ import { AuthService } from '../../services/auth/auth.service';
 export class AuthComponent implements OnInit {
   public email: string = '';
   public password: string = '';
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
@@ -17,7 +21,8 @@ export class AuthComponent implements OnInit {
     this.authService.login(this.email, this.password).subscribe(() => {
       /* TODO: change role assignment */
       this.authService.userInfo
-        ? (this.authService.Role = this.authService.userInfo?.roles.map((item) => item.name))
+        ? (this.authService.Role = this.authService.userInfo?.roles.map((item) => item.name)) &&
+          this.router.navigate(['meetups'])
         : null;
       alert(`current role is ${this.authService.role}`);
     });
